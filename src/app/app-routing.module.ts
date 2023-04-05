c89cc57d92a632169c4a328968cc1bb1662c629e
khadija eddib <khadijaeddib05@gmail.com>
2023-04-05 18:36:41 +0000

    candidates table added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule  } from '@angular/router';
 import { LockScreenComponent } from './components/account/lock-screen/lock-screen.component';
 import { LoginComponent } from './components/account/login/login.component';
 import { SignupComponent } from './components/account/signup/signup.component';
+import { DashboardComponent } from './components/admin/home/home/dashboard/dashboard.component';
 import { HomeComponent } from './components/admin/home/home.component';
+import { CandidatesComponent } from './components/admin/home/home/candidates/candidates.component';
+import { RecruitersComponent } from './components/admin/home/home/recruiters/recruiters.component';
+import { OffersComponent } from './components/admin/home/home/offers/offers.component';
+import { CompaniesComponent } from './components/admin/home/home/companies/companies.component';
+import { CandidaturesComponent } from './components/admin/home/home/candidatures/candidatures.component';
+import { InterviewsComponent } from './components/admin/home/home/interviews/interviews.component';
 
 
 const routes: Routes = [
   { path: 'signup', component: SignupComponent },
   { path: 'login', component: LoginComponent },
   { path: 'lockScreen', component: LockScreenComponent },
-  { path: 'admin', component: HomeComponent },
+  { path: 'admin',
+    component: HomeComponent,
+    children:[
+      { path: 'dashboard', component: DashboardComponent },
+      { path: '',   redirectTo: '/admin/dashboard', pathMatch: 'full' },
+      { path: 'recruiters', component: RecruitersComponent },
+      { path: 'candidates', component: CandidatesComponent },
+      { path: 'offers', component: OffersComponent },
+      { path: 'companies', component: CompaniesComponent },
+      { path: 'candidatures', component: CandidaturesComponent },
+      { path: 'interviews', component: InterviewsComponent }
+    ] },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { SignupComponent } from './components/account/signup/signup.component';
 import {Ng2TelInputModule} from 'ng2-tel-input';
@@ -9,6 +14,13 @@ import { LoginComponent } from './components/account/login/login.component';
 import { LockScreenComponent } from './components/account/lock-screen/lock-screen.component';
 import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './components/admin/home/home.component';
+import { DashboardComponent } from './components/admin/home/home/dashboard/dashboard.component';
+import { CandidatesComponent } from './components/admin/home/home/candidates/candidates.component';
+import { RecruitersComponent } from './components/admin/home/home/recruiters/recruiters.component';
+import { OffersComponent } from './components/admin/home/home/offers/offers.component';
+import { CandidaturesComponent } from './components/admin/home/home/candidatures/candidatures.component';
+import { InterviewsComponent } from './components/admin/home/home/interviews/interviews.component';
+import { CompaniesComponent } from './components/admin/home/home/companies/companies.component';
 
 @NgModule({
   declarations: [
@@ -16,15 +28,34 @@ import { HomeComponent } from './components/admin/home/home.component';
     SignupComponent,
     LoginComponent,
     LockScreenComponent,
-    HomeComponent
+    HomeComponent,
+    DashboardComponent,
+    CandidatesComponent,
+    RecruitersComponent,
+    OffersComponent,
+    CandidaturesComponent,
+    InterviewsComponent,
+    CompaniesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     Ng2TelInputModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpTranslateLoader,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function httpTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
