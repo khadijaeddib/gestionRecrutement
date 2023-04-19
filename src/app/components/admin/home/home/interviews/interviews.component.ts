06ecd5ecaebd2f5c5c4c976cd75bdb533eb2bfdf
khadija eddib <khadijaeddib05@gmail.com>
2023-04-19 16:15:29 +0000

    interviews page added

--- src/app/components/admin/home/home/interviews/interviews.component.ts
+++ src/app/components/admin/home/home/interviews/interviews.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+interface ClassColors {
+  [key: string]: string;
+}
 @Component({
   selector: 'app-interviews',
   templateUrl: './interviews.component.html',
   styleUrls: ['./interviews.component.css']
 })
 export class InterviewsComponent implements OnInit {
+  mySelectedValue: string = 'planned'; // valeur initiale
+
+  classColors: ClassColors = {
+    planned: 'received',
+    done: 'hiring',
+    cancelled: 'refusal'
+  };
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onStatusChange(event: any) {
+    this.mySelectedValue = event.target.value;
   }
 
 }
